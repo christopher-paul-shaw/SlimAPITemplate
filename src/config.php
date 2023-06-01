823ed2e8b9ff8c2b1ec1c95f534a58ad2fe89552
Christopher Shaw <cshaw@pop-os.localdomain>
2023-06-01 11:14:34 +0100

    Add Phinx and DB

--- src/config.php
+++ src/config.php
@@ -1,14 +1,18 @@
 <?php
 
 return [
-    'db' => [
-        'driver' => 'mysql',
-        'host' => '127.0.0.1:3306',
-        'database' => 'example',
-        'username' => 'username',
-        'password' => 'password',
-        'charset'   => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-        'prefix'    => '',
+    'settings' => [
+        'determineRouteBeforeAppMiddleware' => false,
+        'displayErrorDetails' => true,
+        'db' => [
+            'driver' => 'mysql',
+            'host' => '127.0.0.1:3306',
+            'database' => 'example',
+            'username' => 'username',
+            'password' => 'password',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ]
     ]
 ];
--- src/public/index.php
+++ src/public/index.php
@@ -3,6 +3,6 @@ use Slim\Factory\AppFactory;
 require __DIR__ . '/../vendor/autoload.php';
 $app = AppFactory::create();
 $app->addRoutingMiddleware();
-$container = $app->getContainer(); 
+$container = $app->getContainer();
 include __DIR__ . '/../app/Routes.php';
 $app->run();
