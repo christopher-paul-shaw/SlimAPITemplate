94a06428003d16ce74d3a6bc9c9126d1f33a75b0
Christopher Shaw <christopher.shaw@zohomail.com>
2023-06-02 14:38:09 +0100

    Fix App Container Port

--- src/public/index.php
+++ src/public/index.php
@@ -1,6 +1,7 @@
 <?php
 use Slim\Factory\AppFactory;
 require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../settings.php';
 $app = AppFactory::create();
 $app->addRoutingMiddleware();
 $container = $app->getContainer();
---
+++ src/settings.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'settings' => [
+        'determineRouteBeforeAppMiddleware' => false,
+        'displayErrorDetails' => true,
+        'db' => [
+            'driver' => 'mysql',
+            'host' => 'mysql',
+            'database' => 'db',
+            'username' => 'user',
+            'password' => 'password',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ]
+    ]
+];
